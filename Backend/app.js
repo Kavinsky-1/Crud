8db580831265f7777d78904b127e2c9d7b7bd44e
Kavinsky-1 <kavinskycallk@gmail.com>
2024-11-08 20:26:09 -0600

    Delete app.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Conectado a mongo"))
 .catch((error) => console.error("Error en la conexión a mongo", error));
 
